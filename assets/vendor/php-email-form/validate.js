3c2c217b0539481b558fa6dfe12fac658cc9b895
Shiv Singh <shivsingh7150@hotmail.com>
2023-08-22 14:47:48 +0530

    Update validate.js

--- assets/vendor/php-email-form/validate.js
+++ assets/vendor/php-email-form/validate.js
@@ -9,8 +9,12 @@ const handleSubmit = (event) => {
     headers: { "Content-Type": "application/x-www-form-urlencoded" },
     body: new URLSearchParams(formData).toString(),
   })
-    .then(() => { document.getElementById("Contact").innerHTML = "<p>Thank you for reaching out to us. We will get back to you shortly. <br/><strong>If you need a faster response, feel free to contact us at the contact number provided.</strong></p>" })
+    .then(() => {
+      document.getElementById("Contact").innerHTML = "<p>Thank you for reaching out to us. We will get back to you shortly. <br/><strong>If you need a faster response, feel free to contact us at the contact number provided.</strong></p>";
+      window.scrollTo(0, 0);
+    })
     .catch((error) => alert(error));
+
 };
 
 document
